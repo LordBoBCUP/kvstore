3ba4460654b36e366cae9d35af750e01e58575de
LordBoBCUP <lordbobcup@gmail.com>
2021-07-13 21:32:36 +1200

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/lordbobcup/kvstore
+
+go 1.16
+
+require (
+	github.com/bradfitz/slice v0.0.0-20180809154707-2b758aa73013
+	github.com/gofrs/uuid v4.0.0+incompatible
+	go4.org v0.0.0-20201209231011-d4a079459e60 // indirect
+)
---
+++ kvstore.go
@@ -0,0 +1,143 @@
+package store
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"sync"
+	"time"
+
+	"github.com/bradfitz/slice"
+	"github.com/gofrs/uuid"
+)
+
+var (
+	kvstore *DB
+	once sync.Once
+)
+
+type DB struct {
+	DB []Row `json:"DB"`
+}
+
+type Row struct {
+	Id       int       `json:"id"`
+	Username string    `json:"username"`
+	Key      uuid.UUID `json:"key"`
+	Date     time.Time `json:"date"`
+	IpAddress	string `json:"ip_address"`
+}
+
+func GetDB() *DB {
+	once.Do(func() {
+        data, err := ReadFromFile()
+		if err != nil {
+			fmt.Println(err)
+			panic("Unable to read kvstore db.db - Application exiting.",)
+		}
+		kvstore = data
+    })
+	fmt.Println("Here")
+	fmt.Printf("%v+", kvstore)
+    return kvstore
+}
+
+func getNextID() int {
+	if (len(kvstore.DB) < 1) {
+		return 1
+	}
+
+	slice.Sort(kvstore.DB[:], func(i, j int) bool {
+		return kvstore.DB[i].Id < kvstore.DB[j].Id
+	})
+
+	v := kvstore.DB[len(kvstore.DB)-1]
+	return v.Id + 1
+}
+
+func WriteToFile() error {
+	data, err := json.Marshal(kvstore)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile("db.db", data, 0777)
+	return err
+
+}
+
+func ReadFromFile() (*DB, error) {
+	data, err := ioutil.ReadFile("db.db")
+	if err != nil {
+		return nil, err
+	}
+
+	db:= DB{}
+
+	err = json.Unmarshal(data, &db)
+	return &db, err
+}
+
+func ExpireOldKeys() {
+	for i, v := range kvstore.DB {
+		t1 := v.Date.Add(time.Hour * 1)
+		if (time.Now().After(t1)) {
+			// Revoke this user
+			fmt.Printf("Removing Key: %v", v)
+			kvstore.DB = RemoveIndex(kvstore.DB, i)
+		}
+	}
+}
+
+func RemoveIndex(s []Row, index int) []Row {
+    return append(s[:index], s[index+1:]...)
+}
+
+func AddRow(row *Row) error {
+	row.Id = getNextID()
+	row.Date = time.Now()
+
+	if (row.IpAddress == "") {
+		return errors.New("IP Address was not completed")
+	}
+
+	if (row.Key.String() == "") {
+		return errors.New("Key was not completed.")
+	}
+	
+	if (row.Username == "") {
+		return errors.New("Username was not completed.")
+	}
+
+	removeExistingUser(row.Username)
+
+	kvstore.DB = append(kvstore.DB, *row)
+
+	err := WriteToFile()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func removeExistingUser(user string) {
+	fmt.Println("here")
+	for i, v := range kvstore.DB {
+		if (v.Username == user) {
+			fmt.Println("Removing item", i)
+			kvstore.DB = RemoveIndex(kvstore.DB, i)
+		}
+	}
+}
+
+func ValidateLogin(user string, key string) error {
+	ExpireOldKeys()
+	for _, v := range kvstore.DB {
+		if (v.Username == user && v.Key == uuid.FromStringOrNil(key)) {
+			return nil
+		}
+	}
+
+	return errors.New("User and key combination not found in database.")
+}
